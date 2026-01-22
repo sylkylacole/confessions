4ee99aa556c2fea0ed7ad5d53f09b168beec81c1
SylKyla Cole <sylkylacole@gmail.com>
2026-01-21 19:06:09 -0600

    Got the buttons to hide/display on click; Free me from the shackles of javascript

--- public/script.js
+++ public/script.js
@@ -1,1 +1,30 @@
 console.log("Connected");
+
+// Import the buttons and sections via id & classes
+
+const confessButton = document.getElementById("confess-button-1");
+const buttonsClass = document.getElementsByClassName("buttons");
+const confessingClass = document.getElementsByClassName("confessing");
+
+// Build function to hide buttons class and display confessing class
+
+// function timeToConfess() {
+//     console.log("Confess button clicked");
+//     document.querySelectorAll(".buttons").forEach(el => {
+//         el.classList.add("hidden");
+//     });
+// };
+
+// Add event listener to confessButton
+
+// confessButton.addEventListener("click", timeToConfess());
+
+confessButton.addEventListener("click", () => {
+  document.querySelectorAll(".buttons").forEach(el => {
+    el.classList.add("hidden");
+  });
+  document.querySelectorAll(".confessing").forEach(el => {
+    el.classList.remove("hidden");
+  });
+  console.log("Confess button clicked");
+});
