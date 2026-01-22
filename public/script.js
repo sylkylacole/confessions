1b51d63f0c5b8ba64c4aa711d14045a367c0f721
SylKyla Cole <sylkylacole@gmail.com>
2026-01-22 17:40:25 -0600

    Minor changes to js and html; awaiting a solution to get ehe js button to NOT reload the page when an alert pops up

--- public/script.js
+++ public/script.js
@@ -1,23 +1,14 @@
 console.log("Connected");
 
-// Import the buttons and sections via id & classes
+// Import everything via id & classes
 
 const confessButton = document.getElementById("confess-button-1");
 const buttonsClass = document.getElementsByClassName("buttons");
 const confessingClass = document.getElementsByClassName("confessing");
+const confessionForm = document.getElementById("confess-submit");
 
-// Build function to hide buttons class and display confessing class
+// Build function to hide buttons class and display confessing class & add event listener to confessButton
 
-// function timeToConfess() {
-//     console.log("Confess button clicked");
-//     document.querySelectorAll(".buttons").forEach(el => {
-//         el.classList.add("hidden");
-//     });
-// };
-
-// Add event listener to confessButton
-
-// confessButton.addEventListener("click", timeToConfess());
 
 confessButton.addEventListener("click", () => {
   document.querySelectorAll(".buttons").forEach(el => {
@@ -36,3 +27,6 @@ textarea.addEventListener("input", () => {
   textarea.style.height = "auto";              // reset height
   textarea.style.height = textarea.scrollHeight + "px"; // grow
 });
+
+
+// Add event listener for submit button
