a7b565f1e06617e54738ebb95a2892d1732d422d
SylKyla Cole <sylkylacole@gmail.com>
2026-01-23 19:40:41 -0600

    Form works with proper validation! Reloads only when the requirements (not empty + 100 min characters) are met

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ console.log("Connected");
 const confessButton = document.getElementById("confess-button-1");
 const buttonsClass = document.getElementsByClassName("buttons");
 const confessingClass = document.getElementsByClassName("confessing");
-const confessionForm = document.getElementById("confess-submit");
+const confessionSubmit = document.getElementById("confess-submit");
 
 // Build function to hide buttons class and display confessing class & add event listener to confessButton
 
@@ -24,9 +24,27 @@ confessButton.addEventListener("click", () => {
 const textarea = document.getElementById("confession");
 
 textarea.addEventListener("input", () => {
-  textarea.style.height = "auto";              // reset height
-  textarea.style.height = textarea.scrollHeight + "px"; // grow
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
 });
 
 
-// Add event listener for submit button
+// Add event listener for submitting form
+
+const form = document.querySelector("#confession-form");
+
+form.onsubmit = function (e) {
+  const text = textarea.value.trim();
+
+  if (text === "") {
+    e.preventDefault();
+    alert("Empty confessions not allowed.");
+    return;
+  };
+
+  if (text.length < 100) {
+    e.preventDefault();
+    alert("Confession is too short. Suspicious.");
+    return;
+  };
+};
